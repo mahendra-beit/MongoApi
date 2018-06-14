6001dc3d89e1fec4ddc04ef437670b6528d7f0f9
Prajapati <mahendra.beit@gmail.com>
2018-06-14 13:37:58 +0530

    edit code for logger

--- mroute/mroute.js
+++ mroute/mroute.js
@@ -1,18 +1,48 @@
 var express = require('express');
 var route = express.Router();
 var companies = require('../schema/schema');
+
+var logger =require('../logger');
+var emitrer = require("../events/eventhub");
 route.get('/companies/:name',function(req,res){
     let name = req.params.name;
     companies.find({name:name},function(err,data){
         
         if(err){
-            console.log("inside if")
             res.json({});
-        }else{ 
-            console.log("Asdasd insidesss") 
-            console.log(data)          
+        }else{
             res.json(data);
         }
     })    
 });
+
+    /* emit JSON array of anme of companies matchning a pattern */
+
+route.get("/cname/:pattern",function(req,res){
+        companies.find({name:{$regex:req.params.pattern,$options:'i'}},{_id:0,name:1},function(err,data){
+            if(err){                
+                res.json([]);                
+            }else{
+                    if(data.length>= 50)  {
+                        emitrer.emit("more",data.length)
+                    }                
+                res.json(data);
+            }
+        })
+});
+
+/* Update Data Company name */
+route.put("/empcount/:name",function(req,res){
+    let cname = req.params.name;
+    companies.update({name:cname},{$set:req.body},function(err,data){
+        if(err){
+            res.send({result:"not update record"});
+        }else{
+            emitrer.emit('update',cname);
+            res.send({result:"Successfully Updated Data......"});
+        }
+    })
+});
+
+
 module.exports = route;

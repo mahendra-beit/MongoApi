1e5f6b3265a4b3185928dc127bb4365a078e1d8c
Prajapati <mahendra.beit@gmail.com>
2018-06-14 16:08:43 +0530

    access databse for other domain

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ var mroute = require('./mroute/mroute');
 /* for parsing the incoming request */
 app.use(bodyParser.json());
 
+app.use(function(req,res,next) {
+res.header("Access-Control-Allow-Origin","*");
+res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+next();
+}); 
+
+
 var mongoose  = require('mongoose');
 mongoose.connect('mongodb://localhost/capgemini');
 
@@ -14,7 +21,10 @@ db.on('error', console.error.bind(console,'connection error:'));
 
 db.once('open',function(){
     console.log("mongo db connection is open ");
-})
+});
+
+
+
 
 app.listen("4780",function(){
     console.log("server is listenddd")
